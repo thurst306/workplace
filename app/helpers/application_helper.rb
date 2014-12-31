4729eb4b804f83910a9dc2632b939706d1241517
Daraul Harris <thurst306@gmail.com>
2014-12-31 18:10:26 +0000

    Added styles for the active navigation link I also fixed the dashboard header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,9 @@ module ApplicationHelper
     def header(page_header)
         content_for(:header) { page_header }
     end 
+    
+    def cp(path)
+        "active" if current_page?(path)
+    end 
 
 end
